eed2ae3a260f0f98dfaa58184f90f1e7dde414a7
abdulalimsujon <alimsujon12@gmail.com>
2022-10-23 23:44:05 +0600

    dynamic routing using useparams

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/contract' element={<Contract></Contract>}></Route>
